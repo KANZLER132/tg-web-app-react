ed2c34fa6a24438257893dec58c8a7a2a4595561
KANZLER132 <nikita_yakubov@list.ru>
2023-04-05 20:40:30 +0400

    init

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 export function useTelegram() {
     const onClose = () => {
         //tg.close();
-        tg.openTelegramLink('https://core.telegram.org/bots/webapps#initializing-web-apps');
+        tg.openLink('https://core.telegram.org/bots/webapps#initializing-web-apps');
     }
 
     const onToggleButton = () => {
