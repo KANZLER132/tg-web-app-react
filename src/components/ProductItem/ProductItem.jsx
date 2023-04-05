36c73e9c788e001f00d16aef36b1ed5f3cb4edfa
KANZLER132 <nikita_yakubov@list.ru>
2023-04-05 11:53:17 +0400

    init

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from "../Button/Button";
+import "./ProductItem.css";
+
+const ProductItem = ({product, className, atChoice}) => {
+
+    const atChoiceHandler = () => {
+        atChoice(product);
+    }
+
+    return (
+        <div className={'product ' + className}>
+            <div className = {'img'}/>
+            <div className = {'title'}>{product.title}</div>
+            <div className = {'description'}>{product.description}</div>
+            <Button className = {'add-btn'} onClick = {atChoiceHandler}>
+                Выбрать конфигурацию
+            </Button>
+
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products = [
+    {id: '1', title: 'Вилочный погрузчик MITSUBI', description: 'dadada'},
+    {id: '2', title: 'Вилочный погрузчик MITSUBI2', description: 'fafafaf'},
+    {id: '3', title: 'Вилочный погрузчик MITSUBI3', description: 'jajaja'},
+]
+
+const getTitle = (items = []) => {
+    items.forEach(item => {
+        return item.title
+    })
+}
+
+
 const ProductList = () => {
+    const {tg} = useTelegram();
+
+    const [addedItems, setAddedItems] = useState([]);
+
+    const atChoice = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product]
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Посмотреть конфигурацию вилочного погрузчика ${getTitle(newItems)}`
+            })
+        }
+
+
+    }
+
     return (
-        <div>
-            ProductList
+        <div className = {'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product = {item}
+                    atChoice = {atChoice}
+                    className = {'item'}
+                />
+            ))}
         </div>
     );
 };
