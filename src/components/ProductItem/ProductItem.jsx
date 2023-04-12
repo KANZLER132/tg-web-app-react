511f187ca8dc65052b7e42ca36642bc898626b33
KANZLER132 <nikita_yakubov@list.ru>
2023-04-12 15:57:41 +0400

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = ({product, className, atChoice}) => {
     }
     return (
         <div className={'product ' + className}>
-            <img src = {product.img} alt = ""  className = {'img'}/>
+            <img src = {require(product.img)} alt = "" className = {'img'}/>
             <div className = {'title'}>{product.title}</div>
             <div className = {'description'}>{product.description}</div>
             <Button className = {'add-btn'} onClick = {atChoiceHandler}>
